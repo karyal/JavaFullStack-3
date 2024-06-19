45a44cb466d369fd23944243c4c366f6d9377367
Krishna Aryal <info.karyal@gmail.com>
2024-06-19 08:23:07 +0545

    July 19 2024

---
+++ URLReWrite/src/main/java/pkg1/Servlet1.java
@@ -0,0 +1,28 @@
+package pkg1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Servlet1</h1>");
+		out.println("<p><a href='Servlet2'>Servlet2</a></p>");
+		out.close();
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
---
+++ URLReWrite/src/main/java/pkg1/Servlet2.java
@@ -0,0 +1,28 @@
+package pkg1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Servlet2</h1>");
+		out.println("<p><a href='Servlet3'>Servlet3</a></p>");
+		out.close();
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
