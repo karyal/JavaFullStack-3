065523d428b27b1ca12dacf03e64c80eebbfafe7
Krishna Aryal <info.karyal@gmail.com>
2024-06-26 08:07:28 +0545

    July 26 2024

---
+++ Servlet_StudentInfo/backup/sql_script.sql
@@ -0,0 +1,19 @@
+create database dbstudents;
+use dbstudents;
+
+create table students(
+	sid int not null,
+    fullname varchar(50) not null,
+    course varchar(50) not null,
+    section varchar(50) not null,
+    sub1 int not null,
+    sub2 int not null,
+    total int not null,
+    average int not null,
+    result varchar(50) not null
+);
+
+alter table students
+	add constraint pk_sid
+		primary key(sid);
+        
