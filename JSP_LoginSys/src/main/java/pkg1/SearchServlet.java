74e3a53dfbb0295f8631b4ab4b7fc0c7c32389f5
Krishna Aryal <info.karyal@gmail.com>
2024-07-16 08:54:13 +0545

    July 16 2024

--- JSP_LoginSys/src/main/java/pkg1/SearchServlet.java
+++ JSP_LoginSys/src/main/java/pkg1/SearchServlet.java
@@ -1,11 +1,13 @@
 package pkg1;
 
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 public class SearchServlet extends HttpServlet {
@@ -15,10 +17,11 @@ public class SearchServlet extends HttpServlet {
 		//Receive
 		String strSearch = request.getParameter("txtSearch");
 		//boolean result = new Utilities().isNumeric(strSearch);
-		new LoginManager().search(strSearch);
-		//Validate
-		//Process -> Search
-		//Display Result
+		List<User> users = new LoginManager().search(strSearch);
+		
+		//forward to display all users on DisplayUsers.jsp file
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("DisplayUsers.jsp");
+		requestDispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ JSP_LoginSys/src/main/webapp/DisplayUsers.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>All Users</title>
+</head>
+<body>
+<h1>All Users</h1>
+<table width='95%' border=1>
+<tr><td>UID</td><td>NAME</td><td>EMAIL</td><td>PHONE</td><td>USERNAME</td><td>PASSWORD</td><td>USER TYPE</td></tr>
+</table>
+</body>
+</html>
