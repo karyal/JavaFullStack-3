5b89b7176e6f7daf59639f86c27f930336ed54a0
Krishna Aryal <info.karyal@gmail.com>
2024-07-16 07:39:13 +0545

    July 16 2024

---
+++ JSP_LoginSys/src/main/java/pkg1/SearchServlet.java
@@ -0,0 +1,28 @@
+package pkg1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Receive
+		String strSearch = request.getParameter("txtSearch");
+		boolean result = isNumeric(strSearch);
+		
+		//Validate
+		//Process -> Search
+		//Display Result
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
